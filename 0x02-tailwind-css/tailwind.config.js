21962f6a60cdbec058809f1356fcd08e2b61d8a6
Halima Muktar <halimamuk1307@gmail.com>
2025-07-04 23:14:40 +0300

    Create tailwind.config.js

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Scans all HTML & JS files in /src for classes
+  theme: {
+    extend: {}, // Where you'll add custom colors, spacing, fonts, etc.
+  },
+  plugins: [],
+}
